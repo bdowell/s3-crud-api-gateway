e3625312c41b659534f2616fdb789df80be69cdc
Bret Dowell <bret.dowell@gmail.com>
2021-07-28 15:36:35 -0700

    Changes to get.js

--- src/get.js
+++ src/get.js
@@ -4,26 +4,26 @@ const s3 = new AWS.S3();
 const BUCKET_NAME = process.env.FILE_UPLOAD_BUCKET_NAME;
 
 module.exports.handler = async (event) => {
-    console.log(event);
+	console.log(event);
 
-    const response = {
-        isBase64Encoded: false,
-        statusCode: 200,
-    };
+	const response = {
+		isBase64Encoded: false,
+		statusCode: 200,
+	};
 
-    try {
-        const params = {
-            Bucket: BUCKET_NAME,
-            Key: decodeURIComponent(event.pathParameters.fileKey),
-        };
-        const getResult = await s3.getObject(params).promise();
-        response.body = JSON.stringify({message: "Successfully retrieved file from S3."}, getResult);
-    } catch (error) {
-        console.log(error);
-        response.body = JSON.stringify({message: "Failed to get file.", errorMessage: error});
-        response.statusCode = 500;
-    }
+	try {
+		const params = {
+			Bucket: BUCKET_NAME,
+			Key: decodeURIComponent(event.pathParameters.fileKey),
+		};
+		const data = await s3.getObject(params).promise();
 
-    return response;
+		response.body = JSON.stringify({ message: "Successfully retrieved file from S3.", data });
+	} catch (e) {
+		console.error(e);
+		response.body = JSON.stringify({ message: "Failed to get file.", errorMessage: e });
+		response.statusCode = 500;
+	}
 
-}
+	return response;
+};
